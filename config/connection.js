062190b3b113abe6384cbdf45fd75858bcb3963e
LNDavid <ldavid@rmacan.com>
2022-05-23 14:12:03 -0400

    add handlerbars templates and coding

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
-// import Sequelize constructor from library
 const Sequelize = require('sequelize');
 
-// create connection to database, pass in MQL info 
-const sequelize = new Sequelize('process.env.DB_NAME', 'process.env.DB_USER', 'process.env.DB_PASSWORD', {
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-});
+  });
+}
 
 module.exports = sequelize;
